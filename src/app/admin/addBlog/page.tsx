f955f064c07b98ae2d18a7f648636acf8185b959
Luis <luis.soares62@aluno.ifce.edu.br>
2025-03-12 14:11:48 -0300

    Començando o layout da página principal

--- src/app/admin/addBlog/page.tsx
+++ src/app/admin/addBlog/page.tsx
@@ -1,6 +1,11 @@
-import { CirclePlus, SquarePen, Mail } from 'lucide-react'
+"use client"
+import { CirclePlus, SquarePen, Mail, CloudUpload  } from 'lucide-react'
+import { useRef } from 'react'
 
 export default function AddBlog(){
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null)
+
     return (
         <main className="w-full h-screen flex items-start font-poppins">
             <div className="h-screen bg-gray-200 flex flex-col gap-10">
@@ -26,11 +31,59 @@ export default function AddBlog(){
                     </div>
                 </div>
             </div>
-            <div className="w-[1px] h-screen bg-gray-800"></div>
+            <div className="w-[1px] min-h-screen bg-gray-800"></div>
             <div className='h-screen flex-1'>
                 <div className='flex justify-between items-center px-10 py-5 border-b-[2px]'>
                     <span>Painel do administrador</span>
-                    <img src="/perfil.jpeg" width={50} height={50} alt="Luís" className='rounded-4xl' />
+                    <img src="/perfil.jpeg" width={50} height={50} alt="Luís" className='rounded-4xl cursor-pointer' />
+                </div>
+                <div className='w-full pl-16 pt-10'>
+                    <div className='w-96 flex flex-col gap-4'>
+                        <div className='flex flex-col gap-3 w-50'>
+                            <span className='text-lg'>Carregue a Miniatura</span>
+                            <div 
+                                className='w-42 h-20 flex flex-col items-center 
+                                            justify-center bg-gray-100 border-dashed 
+                                            border-2 border-gray-300 cursor-pointer relative'
+                                            onClick={() => {fileInputRef.current?.click()}}>
+                                <CloudUpload  className='text-gray-400 size-9'/>
+                                <span className='text-gray-400 text-sm'>Carregar</span>
+                                <input 
+                                    type="file" 
+                                    ref={fileInputRef} 
+                                    className='absolute right-[9999px]'
+                                    onChange={e => {
+                                        const file = e.target.files?.[0]
+                                        console.log(file)
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className='text-lg'>Título do Blog</span>
+                            <input 
+                                type="text" 
+                                placeholder='Digite aqui' 
+                                className='w-full px-3 py-3 border-[0.5px] border-gray-400 outline-none'
+                            />
+                        </div>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className='text-lg'>Descrição do Blog</span>
+                            <textarea 
+                                placeholder='escreva o conteúdo aqui'
+                                className='w-full h-32 px-3 py-3 border-[0.5px] border-gray-400 outline-none '
+                                />
+                        </div>
+                        <div className='flex flex-col gap-3 w-46'>
+                            <span className='text-lg'>Categoria do Blog</span>
+                            <select className='p-3 border-[0.5px] border-gray-400 outline-none text-gray-500'>
+                                <option value="Startup">Startup</option>
+                                <option value="Tecnológia">Tecnológia</option>
+                                <option value="Empreendorismo">Empreendorismo</option>
+                            </select>
+                        </div>
+                        <button className='w-46 bg-black text-white py-3 cursor-pointer'>ADD</button>
+                    </div>
                 </div>
             </div>
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-import Image from "next/image";
+import { ArrowRight } from 'lucide-react';
+
+import data from "../../data.json"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className='w-full min-h-screen font-poppins '>
+      <header className='w-full h-[20vh]'>
+        <div className="flex items-center justify-between px-12 py-3">
+          <div className='flex items-center cursor-pointer'>
+            <img src="/icon-logo.png" width={66} height={66} alt="Logo" />
+            <span className="text-black text-2xl font-semibold tracking-tighter">NFL's Blogger</span>
+          </div>
+            <div className="flex items-center justify-center px-4 py-2 border-[0.5px] 
+                          border-black text-black text-base cursor-pointer shadow-links hover:shadow-links-move">
+              Começar
+              <ArrowRight />
+            </div>
+        </div>
+      </header>
+      <main className='w-full min-h-[30vh] flex justify-center'>
+        <div className='w-full text-center flex flex-col items-center gap-8'>
+          <h1 className='text-5xl font-semibold'>Últimos Posts</h1>
+          <p className='w-[60%]'>
+            Fique por dentro das últimas novidades em tecnologia, startups e lifestyle. 
+            Explore conteúdos exclusivos, tendências do mercado e insights valiosos para se manter sempre atualizado!
+          </p>
+          <div className='w-[500px] shadow-links'>
+            <input 
+              type="text" 
+              placeholder='Entre com seu e-mail'
+              className='w-[350px] border-[0.5px] border-black px-4 py-2  outline-none'
+            />
+            <button className='w-[150px] border-[0.5px] px-4 py-2 border-black cursor-pointer'>Inscrever-se</button>
+          </div>
+        </div>
+      </main>
+      <section>
+        {data.map((post) => {
+          return (
+            <p>{post.title}</p>
+          )
+        })}
+      </section>
+      <footer></footer>
     </div>
   );
 }
